c15d77c9f3e533baa74127fb0e388cfac82ecdf9
aefuente <aefuente@pm.me>
2025-11-12 07:56:09 -0600

    adding mvzr to project

--- src/main.zig
+++ src/main.zig
@@ -2,14 +2,20 @@ const std = @import("std");
 const zell = @import("zell");
 
 pub fn main() !void {
+    const regex: zell.regex.Regex = zell.regex.compile("^exit$").?;
+
     while (true) {
         var buf: [1024]u8 = undefined;
         var commands: [50][]const u8 = undefined;
         const size = try zell.readInput(&buf);
         std.debug.print("You entered: '{s}'\n", .{ buf[0..size] });
         _ = try zell.parse(buf[0..size], &commands);
-        if (std.mem.eql(u8, commands[0], "exit")) {
+
+
+        if (regex.isMatch(commands[0])) {
             return;
         }
     }
+
+
 }
--- src/root.zig
+++ src/root.zig
@@ -1,8 +1,10 @@
 //! By convention, root.zig is the root source file when making a library.
 const stdin = @import("stdin.zig");
 const parser = @import("parser.zig");
+const mvzr = @import("mvzr.zig");
 
 pub const readInputAllocating = stdin.readInputAllocating;
 pub const readInput = stdin.readInput;
 pub const parseAllocating = parser.parseAllocating;
 pub const parse = parser.parse;
+pub const regex = mvzr;
