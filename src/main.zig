36180383422a25f539179b8e86c8ee377d172db4
aefuente <aefuente@pm.me>
2025-11-12 17:07:18 -0600

    basic external commands

--- src/main.zig
+++ src/main.zig
@@ -2,80 +2,38 @@ const std = @import("std");
 const zell = @import("zell");
 const assert = std.debug.assert;
 
-// pub fn execvpeZ( file: [*:0]const u8, argv_ptr: [*:null]const ?[*:0]const u8, envp: [*:null]const ?[*:0]const u8, ) ExecveError
-const Command  = struct {
-    file: [*:0]const u8,
-    argv_ptr: [*:null]const ?[*:0]const u8,
-    env: [*:null]const ?[*:0]const u8,
-
-};
-
-
-fn create_command(allocator: std.mem.Allocator, line: []const u8) !Command{
-
-    var split_it = std.mem.splitSequence(u8, line, " ");
-    const first = split_it.first();
-
-    var file = try allocator.alloc(u8, first.len + 1);
-    std.mem.copyForwards(u8, file, first);
-    file[first.len] = 0;
-
-    var args = try std.ArrayList(?[*:0]const u8).initCapacity(allocator, 10);
-
-    _ = split_it.next();
-    while (split_it.next()) |arg | {
-        var alarg = try allocator.alloc(u8, arg.len + 1);
-        std.mem.copyForwards(u8, alarg, arg);
-        alarg[arg.len] = 0;
-        try args.append(allocator, @ptrCast(alarg.ptr));
-    }
-
-    const argv_ptr = try args.toOwnedSliceSentinel(allocator, null);
-
-
-    return Command{
-        .file = @ptrCast(file.ptr),
-        .argv_ptr = @ptrCast(argv_ptr.ptr),
-        .env = &[_:null]?[*:0]u8{null},
-    };
-}
 
 pub fn main() !void {
-
-    var debug_allocator: std.heap.DebugAllocator(.{}) = .init;
-    defer assert(debug_allocator.deinit() == .ok);
-    const gpa = debug_allocator.allocator();
-
+ 
+    var buf: [1024]u8 = undefined;
 
     while (true) {
+        const  size = try zell.readInput(&buf);
 
-        const stdin = std.fs.File.stdin();
-        var stdin_read_buf: [100]u8 = undefined;
-        var stdin_reader = stdin.reader(&stdin_read_buf);
-        const reader = &stdin_reader.interface;
+        var args_ptrs: [10:null]?[*:0]u8 = undefined;
+        var i: usize = 0;
+        var n: usize = 0;
+        var ofs: usize = 0;
+        while (i <= size) : (i += 1) {
+            if (buf[i] == 0x20 or i == size) {
+                buf[i] = 0;
+                args_ptrs[n] = @as([*:0]u8, buf[ofs..i :0].ptr);
+                n += 1;
+                ofs = i + 1;
+            }
+        }
 
-        var writer = std.Io.Writer.Allocating.init(gpa);
-        defer writer.deinit();
+        args_ptrs[n] = null;
 
-        _ = try reader.streamDelimiter(&writer.writer, '\n');
-
-        const line = writer.written();
-        std.debug.print("line: {s}\n", .{line});
-
-        const command = try create_command(gpa, line);
-        std.debug.print("command: {any}\n", .{command});
-
-        // Parse/format the input for  the input
-        
         // Fork 
         const fork_pid = try std.posix.fork();
         
         // If child
         if (fork_pid == 0) {
+                const env = [_:null]?[*:0]u8{null};
 
-            const result = std.posix.execvpeZ(command.file, command.argv_ptr, command.env);
+            const result = std.posix.execvpeZ(args_ptrs[0].?, &args_ptrs, &env);
             std.debug.print("Result: {any}", .{result});
-        // pub fn execvpeZ( file: [*:0]const u8, argv_ptr: [*:null]const ?[*:0]const u8, envp: [*:null]const ?[*:0]const u8, ) ExecveError
 
             return;
 
@@ -88,6 +46,4 @@ pub fn main() !void {
 
         }
     }
-
-
 }
--- src/root.zig
+++ src/root.zig
@@ -1,10 +1,5 @@
 //! By convention, root.zig is the root source file when making a library.
 const stdin = @import("stdin.zig");
-const parser = @import("parser.zig");
-const mvzr = @import("mvzr.zig");
 
 pub const readInputAllocating = stdin.readInputAllocating;
 pub const readInput = stdin.readInput;
-pub const parseAllocating = parser.parseAllocating;
-pub const parseAlloc = parser.parseAlloc;
-pub const regex = mvzr;
