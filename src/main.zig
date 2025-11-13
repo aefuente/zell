9f4cf6b6166dee421fc4736c61660f18befb3b20
aefuente <aefuente@pm.me>
2025-11-13 14:46:49 -0600

    allocating writers

--- src/main.zig
+++ src/main.zig
@@ -1,67 +1,51 @@
 const std = @import("std");
-const zell = @import("zell");
+const Allocator = std.mem.Allocator;
+const assert = std.debug.assert;
 
 const builtIns = [_][]const u8 {"cd"};
 
-const ARGS_MAX_SIZE: usize = 10;
-const MAX_INPUT_SIZE: usize = 1024;
-
-fn isBuiltin(command: []const u8) bool {
-    for (builtIns) |builtin| {
-        if (std.mem.eql(u8, builtin, command)) {
-            return true;
-        }
-    }
-    return false;
-}
-
-fn runBuiltin(command: []const u8, args: [:null]const?[*:0]const u8) !void {
-    if (std.mem.eql(u8, command, "cd")) {
-        std.posix.chdirZ(args[1].?) catch | err | {
-            std.debug.print("{any}\n", .{err});
-        };
-    }
-}
 
 pub fn main() !void {
-    var buf: [MAX_INPUT_SIZE]u8 = undefined;
+    var debug_allocator: std.heap.DebugAllocator(.{}) = .init;
+    defer assert(debug_allocator.deinit() == .ok);
+    const gpa = debug_allocator.allocator();
+
+
+    var stdout_buffer: [1024]u8 = undefined;
+    var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
+    const stdout = &stdout_writer.interface;
+
+    var write_buffer = try std.ArrayList(u8).initCapacity(gpa, 50);
+    defer write_buffer.deinit(gpa);
+
+    var arg_buffer = try std.ArrayList(?[*:0]u8).initCapacity(gpa, 10);
+    defer arg_buffer.deinit(gpa);
 
     const env = std.c.environ;
 
     while (true) {
-        const  size = try zell.readInput(&buf);
+        write_buffer.clearRetainingCapacity();
+        arg_buffer.clearRetainingCapacity();
+        try stdout.print("zell>> ",.{});
+        try stdout.flush();
 
-        var failedInput = false;
-        var args_ptrs: [ARGS_MAX_SIZE:null]?[*:0]u8 = undefined;
+        const size = try read_stdin(gpa, &write_buffer);
+
         var i: usize = 0;
         var n: usize = 0;
         var ofs: usize = 0;
         while (i <= size) : (i += 1) {
-            if (i >= MAX_INPUT_SIZE) {
-                std.debug.print("error input too large\n", .{});
-                failedInput = true;
-                break;
-            }
-            if (n >= ARGS_MAX_SIZE) {
-                std.debug.print("error too many arguments\n", .{});
-                failedInput = true;
-                break;
-            }
-            if (buf[i] == 0x20 or i == size) {
-                buf[i] = 0;
-                args_ptrs[n] = @as([*:0]u8, buf[ofs..i :0].ptr);
+            if (write_buffer.items[i] == 0x20 or i == size) {
+                write_buffer.items[i] = 0;
+                try arg_buffer.append(gpa, @as([*:0]u8, write_buffer.items[ofs..i :0].ptr));
                 n += 1;
                 ofs = i + 1;
             }
         }
 
-        if (failedInput) {
-            continue;
-        }
+        try arg_buffer.append(gpa, null);
 
-        args_ptrs[n] = null;
-
-        const command = std.mem.span(args_ptrs[0].?);
+        const command = std.mem.span(arg_buffer.items[0].?);
 
         if (command.len == 0) {
             continue;
@@ -71,14 +55,14 @@ pub fn main() !void {
             return;
         }
 
-        if (isBuiltin(command)) {
-            try runBuiltin(command, &args_ptrs);
+        if (is_builtin(command)) {
+            try run_builtin(command, @ptrCast(arg_buffer.items));
             continue;
         }
 
         const fork_pid = try std.posix.fork();
         if (fork_pid == 0) {
-            const result = std.posix.execvpeZ(args_ptrs[0].?, &args_ptrs, env);
+            const result = std.posix.execvpeZ(arg_buffer.items[0].?, @ptrCast(arg_buffer.items), env);
             std.debug.print("Result: {any}\n", .{result});
 
             return;
@@ -88,5 +72,35 @@ pub fn main() !void {
                 std.debug.print("Command returned {}.\n", .{wait_result.status});
             }
         }
+    }
+}
+
+pub fn read_stdin(allocator: Allocator, array_list: *std.ArrayList(u8)) !usize {
+    var input_buf: [20]u8 = undefined;
+    var stdin = std.fs.File.stdin().reader(&input_buf);
+    var reader = &stdin.interface;
+
+    var allocating_writer = std.Io.Writer.Allocating.fromArrayList(allocator, array_list);
+    const count = try reader.streamDelimiter(&allocating_writer.writer, '\n');
+    array_list.* = allocating_writer.toArrayList();
+    try array_list.append(allocator, 0);
+    return count;
+}
+
+
+fn is_builtin(command: []const u8) bool {
+    for (builtIns) |builtin| {
+        if (std.mem.eql(u8, builtin, command)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+fn run_builtin(command: []const u8, args: [:null]const?[*:0]const u8) !void {
+    if (std.mem.eql(u8, command, "cd")) {
+        std.posix.chdirZ(args[1].?) catch | err | {
+            std.debug.print("{any}\n", .{err});
+        };
     }
 }
--- src/root.zig
+++
@@ -1,4 +0,0 @@
-//! By convention, root.zig is the root source file when making a library.
-const stdin = @import("stdin.zig");
-
-pub const readInput = stdin.readInput;
